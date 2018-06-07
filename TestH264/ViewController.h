a46e96e54b178509523b07eb20877cf17a853df3
gaolele <gaolele@navinfo.com>
2018-06-07 16:29:43 +0800

    Initial Commit

---
+++ TestH264/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestH264
+//
+//  Created by gaol on 2018/6/7.
+//  Copyright © 2018年 gaol. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TestH264/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  TestH264
+//
+//  Created by gaol on 2018/6/7.
+//  Copyright © 2018年 gaol. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
